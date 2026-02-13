39ac29c0783f1d207d539637affc353ae6e3ac93
Valeria Alfonso <a.alfonsos@uniandes.edu.co>
2026-02-13 12:13:31 -0500

    cambios

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+//gcc hello.c -o hello
+//! alerta
+
+//? pregunta
+//*nota
+
+
+int main(){
+    int num;
+    printf("Ingrese un entero: ");
+    scanf("%d",swddsdsd &num);
+    printf("se ingreso un %d", num);
+    return 0;
+    
+
+};
+
